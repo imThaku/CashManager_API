9276d07469e6ba3be2158c0d8aba5f0b0cd81c82
Maxime Wierez-Kien <maxime.wierez-kien@epitech.eu>
2019-12-02 10:54:07 +0100

    Added StatusController

--- src/main/java/CashManager/controllers/StatusController.java
+++ src/main/java/CashManager/controllers/StatusController.java
@@ -1,4 +1,24 @@
 package CashManager.controllers;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StatusController {
+
+    /**
+     * Get the status of the API
+     * @return 200 Http code
+     */
+    @GetMapping("/status")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "API is online"),
+    })
+    public ResponseEntity getStatus() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
